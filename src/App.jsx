e9db2b9c4be6fccd22ee2849b6f8eb865610ce68
anus26 <anusraza268@gmail.com>
2026-01-29 11:50:13 -0800

    customer navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Admin from './pages/Admin'
+import Customer from './pages/Customer'
+import Seller from './pages/Seller'
 
 const Smvecom = () => {
   return (
-    <div className='font-bold  text- m-5 text-center'>Smvecom</div>
+<BrowserRouter>
+<Routes>
+  <Route index element={<Customer/>}/>
+  <Route path='Admin' element={<Admin/>}/>
+  <Route path='Seller' element={<Seller/>}/>
+</Routes>
+</BrowserRouter>
   ) 
 }
 
---
+++ src/pages/Customer/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/customer/navbar'
+const Customer = () => {
+  return (
+<>
+<section>
+    <Navbar/>
+</section>
+</>
+  )
+}
+
+export default Customer
