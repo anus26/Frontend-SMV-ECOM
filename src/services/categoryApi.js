d84ae8f7f75c2e825a4e4af8f6726ca94b394996
anus26 <anusraza268@gmail.com>
2026-02-24 06:33:58 -0800

    modified

--- src/services/categoryApi.js
+++ src/services/categoryApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL:"https://civic-marti-anusraza123bm-c88fd7a4.koyeb.app/api/v1/category",
+    baseURL:"https://colourful-edithe-anusraza123bm-19b0b6f2.koyeb.app/api/v1/category",
     withCredentials:true
 })
 
