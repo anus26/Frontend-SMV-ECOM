a247c0d9196f1143708af6fbcd62c65a705da298
anus26 <anusraza268@gmail.com>
2026-06-24 03:24:52 -0700

    addresstype

--- src/pages/Customer/BuyNow.jsx
+++ src/pages/Customer/BuyNow.jsx
@@ -10,17 +10,55 @@ const BuyNow = () => {
     const  dispatch=useDispatch()
     const {buy,loading,error}=usebuy()
     const userid=buy?.userId
-    const [formData,setFormData]=useState({})
-    const input=[
-        "Full Name",
-        "Province",
-        "City",
-        "Area",
-        "Phone",
-        "Address",
-        "Building/House.No/Floor/Street",
-        "Colony/suburb/Locality/Landmark"
-    ]
+    const [formData,setFormData]=useState({
+FullName:"",
+Province:"",
+City:"",
+Area:"",
+Phone:"",
+Address:"",
+Building:"",
+Colony:"",
+AddressType:""
+
+
+
+})
+
+const input=[
+{
+label:"Full Name",
+name:"FullName"
+},
+{
+label:"Province",
+name:"Province"
+},
+{
+label:"City",
+name:"City"
+},
+{
+label:"Area",
+name:"Area"
+},
+{
+label:"Phone",
+name:"Phone"
+},
+{
+label:"Address",
+name:"Address"
+},
+{
+label:"Building/House.No/Floor/Street",
+name:"Building"
+},
+{
+label:"Colony/suburb/Locality/Landmark",
+name:"Colony"
+}
+]
     const handleChange=(e)=>{
         setFormData({
             ...formData,
@@ -38,28 +76,40 @@ const BuyNow = () => {
     <div className="bg-white rounded-sm shadow-md m-16">
         <h1 className='font-semibold text-xl m-10'>Delivery Information</h1>
      
-    <form >
+    <form onSubmit={handlesubmit}>
 <div className="">
 <div className="grid grid-cols-2 gap-8 m-10">
 
 
 {input.map((item)=>(
-    <div key={item} className="text-text" >
-        <label htmlFor={item} >{item}</label>
-        <input type="text" id={item} name={item} className="border rounded-md p-2 w-full border-gray2 hover:border-text" />
+    <div key={item.name} className="text-text" >
+        <label htmlFor={item} >{item.label}</label>
+        <input onChange={handleChange} value={formData[item.name]}  type="text" id={item} name={item.name} className="border rounded-md p-2 w-full border-gray2 hover:border-text" />
     </div>
 ))}
 </div>
  <div className='  m-10'>
 <h1  className='flex justify-end mr-10 font-semibold'>Select a label for this address</h1>
 <div className='flex gap-3 justify-end'>
-
-<button className='flex items-center gap-2 p-4 bg-white text-black shadow-md m-3 rounded-md border-green border transition duration-300 hover '>
-  <IoBagRemoveOutline  />  Office 
+   
+<button type='button' onClick={()=>setFormData({
+    ...formData,AddressType:"Office"
+})} className={`flex items-center gap-2 p-4 bg-white text-black shadow-md m-3 rounded-md border-green border ${formData.AddressType==="Office" 
+? "bg-green text-white" 
+: "bg-white text-black"}`}>
+  <IoBagRemoveOutline  />  Office
 </button>
-<button className='flex items-center gap-2 p-4 bg-white text-black shadow-md m-3 rounded-md border-red1 border transition duration-300 hover '>
+<button type="button" onClick={()=>setFormData({
+    ...formData,AddressType:"Home"
+})} className={`flex items-center gap-2 p-4 bg-white text-black shadow-md m-3 rounded-md border-red1 border ${formData.AddressType==="Home" 
+? "bg-red1 text-white" 
+: "bg-white text-black"}`}>
    <IoHomeOutline /> Home
 </button>
+
+    
+  
+
 </div>
     </div>
 
